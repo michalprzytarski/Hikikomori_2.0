2f5b9501e091946bd150b379373dd0780f7f5235
MiloszTarka <82045142+MiloszTarka@users.noreply.github.com>
2021-10-15 23:38:42 +0200

    Home screen first iteration
    Home screen first iteration

--- App/hikikomori/src/App.js
+++ App/hikikomori/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./components/screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <Home></Home>
       </Router>
     </div>
   );
