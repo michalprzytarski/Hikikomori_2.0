b1753f37cfc64e5505ac060fc4a738b17b7cce71
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2021-10-16 01:56:40 +0200

    created prototype quiz

--- App/hikikomori/src/components/screens/AppointmentScreen.jsx
+++ App/hikikomori/src/components/screens/AppointmentScreen.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { questions } from "../../constants/AppointmentQuestions";
 
 export default function AppointmentScreen() {
+  const [currentQuestion, setCurrentQuestion] = useState(
+    findQuestion(`initial`)
+  );
+
+  useEffect(() => {
+    setCurrentQuestion(findQuestion(`initial`));
+    return () => {};
+  }, []);
+
+  function findQuestion(questionName) {
+    console.log(questions.find((x) => x.name === questionName));
+    return questions.find((x) => x.name === questionName);
+  }
+
+  function printQuestion(question) {
+    return (
+      <div>
+        <h3>{question.question}</h3>
+        <ul>
+          {question.answers.map((answer) => (
+            <li>{answer.answer}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  function handleQuiz(question) {
+    if (question.finalAnswer !== undefined) {
+      return <h1>{question.finalAnswer}</h1>;
+    } else
+      return (
+        <div>
+          <h3>{question.question}</h3>
+          {question.answers.map((answer) => (
+            <button
+              onClick={() =>
+                setCurrentQuestion(() => findQuestion(answer.nextQuestion))
+              }
+            >
+              {answer.answer}
+            </button>
+          ))}
+        </div>
+      );
+  }
   return (
     <div>
       <h1>AppointmentScreen</h1>
+      {handleQuiz(currentQuestion)}
     </div>
   );
 }
---
+++ App/hikikomori/src/constants/AppointmentQuestions.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const questions = [
+  {
+    name: `initial`,
+    question: "How  can I help you today?",
+    finalAnswer: undefined,
+    answers: [
+      {
+        answer: `I'm injured`,
+        nextQuestion: `injured`,
+      },
+      {
+        answer: `I'm feeling some kind of pain`,
+        nextQuestion: `pain`,
+      },
+    ],
+  },
+  {
+    name: `injured`,
+    question: undefined,
+    finalAnswer: `Call an ambulance as soon as posible!!!`,
+    answers: undefined,
+  },
+  {
+    name: `pain`,
+    question: `What exactly does hurt you?`,
+    finalAnswer: undefined,
+    answers: [
+      {
+        answer: `Head`,
+        nextQuestion: `head`,
+      },
+      {
+        answer: "Leg",
+        nextQuestion: `leg`,
+      },
+    ],
+  },
+  {
+    name: `head`,
+    question: undefined,
+    finalAnswer: "You should visit neurologist.",
+    answers: undefined,
+  },
+  {
+    name: `leg`,
+    question: undefined,
+    finalAnswer: `You should visit orthopaedist`,
+    answers: undefined,
+  },
+];
