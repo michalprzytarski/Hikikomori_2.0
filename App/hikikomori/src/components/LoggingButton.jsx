ceb125d455abbc4f7e36fd181cba2aa729d18e71
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2021-10-16 14:55:29 +0200

    styled logout button

--- App/hikikomori/src/components/LoggingButton.jsx
+++ App/hikikomori/src/components/LoggingButton.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "../userContext";
 import { NavLink } from "react-router-dom";
 import { loginScreen } from "./screens/screens";
+import "./LoggingButton.css";
 
 export default function LoggingButton() {
   const { username, setUsername } = useContext(UserContext);
@@ -9,13 +10,14 @@ export default function LoggingButton() {
     return (
       <div>
         <h5>Hello, {username}</h5>
-        <button
+        <div
+          className="LogoutButton"
           onClick={() => {
             setUsername(undefined);
           }}
         >
           Wyloguj
-        </button>
+        </div>
       </div>
     );
   else
